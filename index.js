c8eef1907c804facc90d8a3f014333701a24d85c
lbernice <bernicel677@gmail.com>
2023-10-23 00:04:49 -0400

    gg

--- index.js
+++ index.js
@@ -87,28 +87,50 @@ const titles = [
 //Array of authors and the book they wrote
 //"--- wrote --- in ---"
 books.forEach((book) => console.log(book.authorFirst + " " + book.authorLast + " " + "wrote" + " " + book.name + " " + "in" + " " + book.publishDate))
+
 //Sort books from oldest to most recent
 books.sort((a, b) => a.publishDate - b.publishDate)
 books.forEach((book) => {
   console.log(book.name);
 })
+
 //sort books alphabetically
 titles.sort();
 console.log(titles);
+
 //Find who wrote War and Peace
 function warPeace(book) {
   return book.name === "War and Peace";
 }
 console.log(books.find(warPeace));
+
 //how many books were written before 1900?
 const result = books.filter((books) => books.publishDate < 1900);
 console.log(result);
+
+//for number of books 
+const booknumber = books.filter((book) => book.publishDate < 1900);
+console.log(booknumber.length);
+
 //was there at least one book published within the last 100 years?
-const date = (value) => value.publishDate > 1923;
-console.log(books.some(date));
+// const date = (value) => value.publishDate > 1923;
+// console.log(books.some(date));
+
+if (books.some((book) => new Date().getFullYear() - book.publishDate <= 100)) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
+
 //was every book published within the last 100 years?
-const publish = (value) => value.publishDate > 1923
-console.log(books.every(publish));
+// const publish = (value) => value.publishDate > 1923
+// console.log(books.every(publish));
+if (books.every((book) => new Date().getFullYear() - book.publishDate <= 100)) {
+  console.log("Yes");
+} else {
+  console.log("No");
+}
+
 //print a list of books that "includes" the genre historical
 books.filter((book) => book.genre.includes("historical"))
 books.forEach((book) => {
